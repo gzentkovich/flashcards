0f073fe98280bd56410e8a4165c27024ef4870f7
gzentkovich <gzentkovich@gmail.com>
2013-05-16 17:28:05 -1000

    add links and crud feature

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -4,4 +4,43 @@ class DecksController < ApplicationController
     @decks = Deck.all
   end
 
+  def show
+    @deck = Deck.find(params[:id])
+  end
+
+  def new
+    @deck = Deck.new
+  end
+
+  def create
+    @deck = Deck.new(params[:deck])
+
+    if @deck.save
+      redirect_to "/decks", notice: 'Decks was successfully created.'
+    else
+      render action: "new"
+    end
+  end
+
+  def edit
+    @deck = Deck.find(params[:id])
+  end
+
+  def update
+    @deck = Deck.find(params[:id])
+
+    if @deck.update_attributes(params[:deck])
+      #redirect_to "/decks/#{@deck.id}", notice: "The #{@deck.name} was successfully updated."
+      redirect_to "/decks", notice: "The #{@deck.name} was successfully updated."
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @deck = Deck.find(params[:id])
+    @deck.destroy
+    redirect_to root_url, notice: "The #{@deck.name} has been successfully deleted."
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Flashcards::Application.routes.draw do
   root :to => "decks#index"
+  get "/decks/new" => "decks#new"
   get "/decks" => "decks#index"
+  get "/decks/:id/edit" => "decks#edit"
+  delete "/decks/:id" => "decks#destroy"
+  get "/decks/:id" => "decks#show", as: :deck
+  post "/decks" => "decks#create"
+  put "/decks/:id" => "decks#update"
 end
