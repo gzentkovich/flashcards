bba57293fe507634d21dcf75fdff77891e1a8d50
gzentkovich <gzentkovich@gmail.com>
2013-05-27 15:41:18 -1000

    add card controller new show

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,19 @@
 class CardsController < ApplicationController
 
   def show
-    @deck = Deck.find(params[:deck_id])
+    @deck = find_deck
     @card = @deck.cards.find(params[:id])
+  end
+
+  def new
+    @deck = find_deck
+    @card = @deck.cards.new
+  end
+
+  private
+
+  def find_deck
+    Deck.find(params[:deck_id])
   end
 
 end
